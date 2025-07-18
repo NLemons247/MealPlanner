d8af57738aed1ab436e25dc11f1b0b1891c0fcdf
Nathaniel Lemons <nate.lemons247@gmail.com>
2025-07-18 18:52:28 -0500

    setting up express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
 require('dotenv').config();
+
+const connect = require("./connect");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    connect.connectToServer();
+    console.log(`Server running on PORT ${PORT}`)
+});
+
+
